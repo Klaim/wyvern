a7c968a92e9a92d792d707532e9ca0ce1c19b090
Klaim (Joel Lamotte) <mjklaim@gmail.com>
2020-08-18 16:02:19 +0200

    WIP

--- libwyvern/libwyvern/wyvern.cpp
+++ libwyvern/libwyvern/wyvern.cpp
@@ -1,11 +1,106 @@
+#include <iostream>
 #include <libwyvern/wyvern.hpp>
 #include <nlohmann/json.hpp>
 #include <libbutl/process.mxx>
+#include <libbutl/path.mxx>
+#include <fmt/core.h>
+
+using json = nlohmann::json;
+
+namespace wyvern {
+
+  struct Logger
+  {
+    Logger() = default;
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    template<class Arg>
+    friend auto operator<<(Logger&& logger, Arg&& to_log)
+      -> Logger&&
+    {
+      std::cout << fmt::format("wyvern: {}", std::forward<Arg>(to_log));
+      return std::move(logger);
+    }
+
+    ~Logger()
+    {
+      std::cout << std::endl;
+    }
+  };
+
+  auto log() -> Logger { return {}; }
+
+}
+
+namespace wyvern::cmake {
+
+  enum class cmakefile_mode
+  {
+    without_dependencies,
+    with_dependencies,
+  };
+
+  auto generate_cmakefile_code(const Configuration& cmake_config, cmakefile_mode mode)
+    -> std::string // content of the CMakeFile.txt
+  {
+    return "empty!";
+  }
+
+  auto create_cmake_project(std::string directory_path, const Configuration& cmake_config, cmakefile_mode mode)
+    -> void
+  {
+    // 1. create the cmakefile with the right content
+    // 2. create main.cpp and header.hpp
+  }
+
+  constexpr auto cmake_file_api_query_json = R"JSON(
+{
+  "requests" : [
+      {
+          "kind": "codemodel",
+          "version": { "major": 2 }
+      }
+  ]
+}
+)JSON";
+
+  auto invoke_cmake(std::vector<std::string> args) -> void
+  {
+    // const auto cmake_command = fmt::format("cmake {}", directory_path);
+    // run the command cmake
+    // throw if any error is found
+  }
+
+  auto query_cmake_file_api(std::string build_directory_path)
+    -> json
+  {
+    // 1. write the query file in the build directory
+    // 2. invoke cemake in that directory
+    invoke_cmake({ build_directory_path });
+    return {};
+  }
+
+  auto configure_project(std::string project_path, std::string build_path, const Configuration& cmake_config)
+  {
+    const auto source_arg = fmt::format("-S {}", project_path);
+    const auto build_dir_arg = fmt::format("-B {}", build_path);
+
+    auto args = std::vector{ source_arg, build_dir_arg };
+    // add args from config
+    invoke_cmake(args);
+  }
+
+
+}
+
 
 namespace wyvern
 {
+
   DependenciesInfo extract_dependencies(const cmake::Configuration& config)
   {
+    log() << "Begin cmake dependencies extraction";
     // 1. Create a temporary cmake project with CMakeFiles.txt and an c++ source file.
     //    It should have no dependencies at all, just as many executable targets as
     //    the number of targets in the provided configuration.
@@ -22,6 +117,8 @@ namespace wyvern
     //    JSON information -> B.
     // 7. Compare A and B, find what's in B that was not in B.
     // Return the result of that comparison.
+
+    log() << "End cmake dependencies extraction";
 
     return {};
   }
--- libwyvern/libwyvern/wyvern.hpp
+++ libwyvern/libwyvern/wyvern.hpp
@@ -18,6 +18,7 @@ namespace wyvern::cmake {
 
   struct Configuration
   {
+    std::string generator; // Name of the generator provided by CMake (that will create the proper build-system files).
     std::vector<Package> packages; // CMake packages to extract informations from.
     std::vector<std::string> targets; // Qualified names of CMake targets to extract information from.
     std::vector<Option> options; // CMake options and variables to pass to CMake on invokation.
@@ -28,16 +29,29 @@ namespace wyvern::cmake {
 
 namespace wyvern
 {
-  struct TargetInfo
+  struct Target
   {
     std::string name;
     std::vector<std::string> include_directories;
-    std::vector<std::string> lib_directories;
+    std::vector<std::string> libraries_directories;
+    std::vector<std::string> link_libraries;
     std::vector<std::string> compilation_flags;
-    std::vector<std::string> files;
+    std::vector<std::string> link_flags;
+    std::vector<std::string> source_files;
   };
 
-  using DependenciesInfo = std::vector<TargetInfo>;
+  struct Configuration
+  {
+    std::string name;
+    std::string directory;
+    std::vector<Target> targets;
+  };
+
+  struct DependenciesInfo
+  {
+    std::vector<Configuration> configurations; // There can be more than one configuration generated, for example with Visual Studio generators.
+    bool empty() const { return configurations.empty(); }
+  };
 
   LIBWYVERN_SYMEXPORT
   DependenciesInfo extract_dependencies(const cmake::Configuration& config);
