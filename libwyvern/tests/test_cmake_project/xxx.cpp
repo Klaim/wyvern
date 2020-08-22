fa04f4d0d4761fd2cb5b2406c5448390cee29f2b
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2020-08-22 13:50:41 +0200

    WIP

---
+++ libwyvern/tests/test_cmake_project/xxx.cpp
@@ -0,0 +1,9 @@
+
+#include "test_project.hpp"
+
+namespace test_cmake_project {
+    void function_xxx()
+    {
+
+    }
+}
---
+++ libwyvern/tests/test_cmake_project/yyy.cpp
@@ -0,0 +1,8 @@
+#include "test_project.hpp"
+
+namespace test_cmake_project {
+    void function_yyy()
+    {
+
+    }
+}
